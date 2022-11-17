463d4d2c5abe4261fb9b400f83dfcffd46bf1f5e
iamyajat <68477362+iamyajat@users.noreply.github.com>
2022-11-17 22:11:33 +0530

    feat: model updated

--- src/summary.py
+++ src/summary.py
@@ -2,7 +2,7 @@ from simplet5 import SimpleT5
 
 model = SimpleT5()
 model.load_model(
-    "t5", "./assets/t5-epoch-9-train-loss-0.517-val-loss-1.9625", use_gpu=False
+    "t5", "./assets/model", use_gpu=False
 )
 
 
